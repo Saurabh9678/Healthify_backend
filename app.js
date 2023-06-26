8e2604286c57e4d6ae2aff0e75ff467c5b2f1e0a
Saurabh <gohainsaurabh@gmail.com>
2023-06-26 20:26:18 +0530

    Working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const errorMiddleware = require("./middleware/error");
 
+
 const app = express();
 
 app.use(express.json());
--- models/userModel.js
+++ models/userModel.js
@@ -88,6 +88,7 @@ userSchema.methods.getJWTToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE,
   });
+  
 };
 
 // Compare Password
